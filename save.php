2ce3477def55b514c1b02c41d33f53ffa0f6dfbb
siddhiparte24 <siddhiparte24@gmail.com>
2022-08-23 15:39:12 +0530

    initial commit

---
+++ save.php
@@ -0,0 +1,17 @@
+<?php
+
+$connection = require_once './Connection.php';
+
+// echo "<pre>";
+// var_dump($_POST);
+// echo "</pre>";
+
+$id = $_POST["id"] ?? '';
+
+if ($id){
+    $connection->updateNote($id, $_POST);
+} else {
+    $connection->addNote($_POST);
+}
+
+header('Location: index.php');
